7404dbc6e398b2419c05ab742a6d2bcec72940c2
Pamela <jpamelapz02@gmail.com>
2021-02-25 17:28:19 -0600

    filex en mayusculas

--- database/seeds/PlansTableSeeder.php
+++ database/seeds/PlansTableSeeder.php
@@ -13,16 +13,15 @@ class PlansTableSeeder extends Seeder
     public function run()
     {
         DB::table('plans')->insert([
-            'title' => 'Plan Filex Premium',
+            'title' => 'Plan FILEX Premium',
             'identifier' => 'Premium',
             'stripe_id' => 'price_1IDFbPKYWi6uPg51ZU3knGTj',
         ]);
 
         DB::table('plans')->insert([
-            'title' => 'Plan Filex Básico',
+            'title' => 'Plan FILEX Básico',
             'identifier' => 'Básico',
             'stripe_id' => 'price_1IDFOaKYWi6uPg51beIFrspu',
-
         ]);
     }
 }
